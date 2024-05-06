ee4a4f356bc9cb000578629c5b04f9e7b0145de6
Alvin Okafor <alvin2k99@gmail.com>
2024-05-06 19:24:27 +0100

    fix lint errors

--- src/pages/orderPage.tsx
+++ src/pages/orderPage.tsx
@@ -1,7 +1,7 @@
 import {
   Box,
   Button,
-  Select,
+  // Select,
   Flex,
   Table,
   TextField,
@@ -10,204 +10,204 @@ import {
 } from "@radix-ui/themes";
 import { useState, useEffect } from "react";
 import AppLayout from "@/layouts/AppLayout";
-import { GetApp } from "@mui/icons-material";
-import { AcceptOrder, CancelOrder, MarkAsPicked } from "@/components/orders";
+// import { GetApp } from "@mui/icons-material";
+// import { AcceptOrder, CancelOrder, MarkAsPicked } from "@/components/orders";
 import { toast, Bounce } from "react-toastify";
 import { getOrders } from "@/config/apiCall";
 import { IOrder } from "@/config/apiCall";
 
-const OverviewTab = () => {
-  const [showMap, setShowMap] = useState(false);
-  return (
-    <>
-      <Box className="p-6 bg-white max-w-[550px] mx-auto rounded-md shadow-md mb-10">
-        {/* <div className="card" style={{ backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '8px', maxWidth: '100%',marginBottom:"40px" }}> */}
+// const OverviewTab = () => {
+//   const [showMap, setShowMap] = useState(false);
+//   return (
+//     <>
+//       <Box className="p-6 bg-white max-w-[550px] mx-auto rounded-md shadow-md mb-10">
+//         {/* <div className="card" style={{ backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '8px', maxWidth: '100%',marginBottom:"40px" }}> */}
 
-        <div
-          className="details"
-          style={{ display: "flex", flexDirection: "column" }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <p
-              className="item"
-              style={{ marginBottom: "10px", color: "rgb(54, 118, 200)" }}
-            >
-              Men's Clothing
-            </p>
-            <p className="price" style={{ marginBottom: "10px" }}>
-              NGN 2,000.00
-            </p>
-          </div>
-          <hr style={{ margin: "20px" }} />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <p className="item" style={{ marginBottom: "10px" }}>
-              Total
-            </p>
-            <p className="price" style={{ marginBottom: "10px" }}>
-              NGN 2,000.00
-            </p>
-          </div>
-          <hr style={{ margin: "20px" }} />
-          <p style={{ marginBottom: "15px", fontWeight: "500" }}>
-            Patrick Francis
-          </p>
-          <p style={{ marginBottom: "15px", color: "#57584e" }}>
-            francisdaniel140@gmail.com
-          </p>
-          <p style={{ marginBottom: "15px", color: "#57584e" }}>
-            +23408092363468
-          </p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <a
-              href="#"
-              className="view-btn"
-              style={{
-                color: "#007bff",
-                border: "none",
-                marginTop: "10px",
-                cursor: "pointer",
-              }}
-            >
-              View Customer
-            </a>
-            <a
-              href="#"
-              className="view-btn"
-              style={{
-                color: "#007bff",
-                border: "none",
-                marginTop: "10px",
-                cursor: "pointer",
-              }}
-            >
-              View Transaction
-            </a>
-            {/* <a href='#' className="view-btn" style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '8px 16px', marginTop: '10px', cursor: 'pointer', borderRadius: '4px' }}>View Customer</a>
-    <a href='#' className="view-btn" style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '8px 16px', marginTop: '10px', cursor: 'pointer', borderRadius: '4px' }}>View Transaction</a>
-    */}
-          </div>
-        </div>
-        {/* </div> */}
-      </Box>
-      <Box className="p-6 bg-white  max-w-[550px] mx-auto rounded-md shadow-md">
-        <div
-          className="details"
-          style={{ display: "flex", flexDirection: "column" }}
-        >
-          <p style={{ marginBottom: "10px", fontWeight: "500" }}>
-            Received On:
-          </p>
-          <p style={{ marginBottom: "15px", color: "gray" }}>
-            Feb 7, 2024, 1:49 PM
-          </p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <CancelOrder />
-            <AcceptOrder setShowMap={setShowMap} />
-            <MarkAsPicked />
-          </div>
-          {showMap && (
-            <Box className="mt-4">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3191.708801974967!2d73.13016848451862!3d22.285120636635604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1705563079752!5m2!1sen!2sin"
-                width="450"
-                height="350"
-                loading="lazy"
-                style={{ border: "0" }}
-              ></iframe>
-            </Box>
-          )}
-        </div>
-        {/* </div> */}
-      </Box>
-    </>
-  );
-};
+//         <div
+//           className="details"
+//           style={{ display: "flex", flexDirection: "column" }}
+//         >
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-between",
+//             }}
+//           >
+//             <p
+//               className="item"
+//               style={{ marginBottom: "10px", color: "rgb(54, 118, 200)" }}
+//             >
+//               Men's Clothing
+//             </p>
+//             <p className="price" style={{ marginBottom: "10px" }}>
+//               NGN 2,000.00
+//             </p>
+//           </div>
+//           <hr style={{ margin: "20px" }} />
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-between",
+//             }}
+//           >
+//             <p className="item" style={{ marginBottom: "10px" }}>
+//               Total
+//             </p>
+//             <p className="price" style={{ marginBottom: "10px" }}>
+//               NGN 2,000.00
+//             </p>
+//           </div>
+//           <hr style={{ margin: "20px" }} />
+//           <p style={{ marginBottom: "15px", fontWeight: "500" }}>
+//             Patrick Francis
+//           </p>
+//           <p style={{ marginBottom: "15px", color: "#57584e" }}>
+//             francisdaniel140@gmail.com
+//           </p>
+//           <p style={{ marginBottom: "15px", color: "#57584e" }}>
+//             +23408092363468
+//           </p>
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-between",
+//             }}
+//           >
+//             <a
+//               href="#"
+//               className="view-btn"
+//               style={{
+//                 color: "#007bff",
+//                 border: "none",
+//                 marginTop: "10px",
+//                 cursor: "pointer",
+//               }}
+//             >
+//               View Customer
+//             </a>
+//             <a
+//               href="#"
+//               className="view-btn"
+//               style={{
+//                 color: "#007bff",
+//                 border: "none",
+//                 marginTop: "10px",
+//                 cursor: "pointer",
+//               }}
+//             >
+//               View Transaction
+//             </a>
+//             {/* <a href='#' className="view-btn" style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '8px 16px', marginTop: '10px', cursor: 'pointer', borderRadius: '4px' }}>View Customer</a>
+//     <a href='#' className="view-btn" style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '8px 16px', marginTop: '10px', cursor: 'pointer', borderRadius: '4px' }}>View Transaction</a>
+//     */}
+//           </div>
+//         </div>
+//         {/* </div> */}
+//       </Box>
+//       <Box className="p-6 bg-white  max-w-[550px] mx-auto rounded-md shadow-md">
+//         <div
+//           className="details"
+//           style={{ display: "flex", flexDirection: "column" }}
+//         >
+//           <p style={{ marginBottom: "10px", fontWeight: "500" }}>
+//             Received On:
+//           </p>
+//           <p style={{ marginBottom: "15px", color: "gray" }}>
+//             Feb 7, 2024, 1:49 PM
+//           </p>
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-between",
+//             }}
+//           >
+//             <CancelOrder />
+//             <AcceptOrder setShowMap={setShowMap} />
+//             <MarkAsPicked />
+//           </div>
+//           {showMap && (
+//             <Box className="mt-4">
+//               <iframe
+//                 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3191.708801974967!2d73.13016848451862!3d22.285120636635604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1705563079752!5m2!1sen!2sin"
+//                 width="450"
+//                 height="350"
+//                 loading="lazy"
+//                 style={{ border: "0" }}
+//               ></iframe>
+//             </Box>
+//           )}
+//         </div>
+//         {/* </div> */}
+//       </Box>
+//     </>
+//   );
+// };
 
-const FilterSearchExport = () => (
-  <Table.Header>
-    <Table.Row>
-      <Table.RowHeaderCell>
-        {/* Filters */}
-        <Select.Root defaultValue="Filter:">
-          <Select.Trigger />
-          <Select.Content>
-            <Select.Group>
-              <Select.Label>Fruits</Select.Label>
-              <Select.Item value="Filter:">Filter:</Select.Item>
-              <Select.Item value="apple">Apple</Select.Item>
-              <Select.Item value="grape" disabled>
-                Grape
-              </Select.Item>
-            </Select.Group>
-            <Select.Separator />
-            <Select.Group>
-              <Select.Label>Vegetables</Select.Label>
-              <Select.Item value="carrot">Carrot</Select.Item>
-              <Select.Item value="potato">Potato</Select.Item>
-            </Select.Group>
-          </Select.Content>
-        </Select.Root>
-      </Table.RowHeaderCell>
-      <Table.RowHeaderCell>
-        {/* Search input */}
-        <TextField.Root>
-          <TextField.Input placeholder="Search order id or customer...." />
-        </TextField.Root>
-      </Table.RowHeaderCell>
-      <Table.RowHeaderCell>
-        {/* Export to CSV button */}
-        <Button variant="solid">
-          <GetApp />
-          Export
-        </Button>
-      </Table.RowHeaderCell>
-      <Table.RowHeaderCell>
-        {/* Dropdown icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="gray"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          role="img"
-          aria-label="more vertical"
-          className="sc-gLDzan iqLDnp"
-          style={{ width: "24px", height: "24px" }}
-        >
-          <circle cx="12" cy="12" r="2"></circle>
-          <circle cx="12" cy="4" r="2"></circle>
-          <circle cx="12" cy="20" r="2"></circle>
-        </svg>
-      </Table.RowHeaderCell>
-    </Table.Row>
-  </Table.Header>
-);
+// const FilterSearchExport = () => (
+//   <Table.Header>
+//     <Table.Row>
+//       <Table.RowHeaderCell>
+//         {/* Filters */}
+//         <Select.Root defaultValue="Filter:">
+//           <Select.Trigger />
+//           <Select.Content>
+//             <Select.Group>
+//               <Select.Label>Fruits</Select.Label>
+//               <Select.Item value="Filter:">Filter:</Select.Item>
+//               <Select.Item value="apple">Apple</Select.Item>
+//               <Select.Item value="grape" disabled>
+//                 Grape
+//               </Select.Item>
+//             </Select.Group>
+//             <Select.Separator />
+//             <Select.Group>
+//               <Select.Label>Vegetables</Select.Label>
+//               <Select.Item value="carrot">Carrot</Select.Item>
+//               <Select.Item value="potato">Potato</Select.Item>
+//             </Select.Group>
+//           </Select.Content>
+//         </Select.Root>
+//       </Table.RowHeaderCell>
+//       <Table.RowHeaderCell>
+//         {/* Search input */}
+//         <TextField.Root>
+//           <TextField.Input placeholder="Search order id or customer...." />
+//         </TextField.Root>
+//       </Table.RowHeaderCell>
+//       <Table.RowHeaderCell>
+//         {/* Export to CSV button */}
+//         <Button variant="solid">
+//           <GetApp />
+//           Export
+//         </Button>
+//       </Table.RowHeaderCell>
+//       <Table.RowHeaderCell>
+//         {/* Dropdown icon */}
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           viewBox="0 0 24 24"
+//           fill="none"
+//           stroke="gray"
+//           stroke-width="2"
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           role="img"
+//           aria-label="more vertical"
+//           className="sc-gLDzan iqLDnp"
+//           style={{ width: "24px", height: "24px" }}
+//         >
+//           <circle cx="12" cy="12" r="2"></circle>
+//           <circle cx="12" cy="4" r="2"></circle>
+//           <circle cx="12" cy="20" r="2"></circle>
+//         </svg>
+//       </Table.RowHeaderCell>
+//     </Table.Row>
+//   </Table.Header>
+// );
 
 const TransactionTable = () => {
   const [allOrders, setAllOrders] = useState<IOrder[]>([]);
@@ -271,7 +271,7 @@ const TransactionTable = () => {
         </Table.Header>
         <Table.Body>
           {/* Table rows */}
-          {allOrders.map((order, index) => (
+          {allOrders.map((order) => (
             <Table.Row key={order._id}>
               {/* <Table.Cell>
                 <CheckBoxOutlineBlank />
